fb7a7a6cbbcb31c6afc7dda2a582a8d4f6daebdd
rockintuna <rockintuna@naver.com>
2025-05-30 22:42:42 +0900

    Initial commit

---
+++ src/main/java/me/rockintuna/smtpdemo/SmtpDemoApplication.java
@@ -0,0 +1,13 @@
+package me.rockintuna.smtpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rockintuna/smtpdemo/SmtpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.smtpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
